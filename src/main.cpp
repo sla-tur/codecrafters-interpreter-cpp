7dfe19f76f951f328b6b2d1fe3e408eb650a1cc4
sla-tur <sla.tur1999@gmail.com>
2025-01-17 14:35:01 +0000

    first commit

--- src/main.cpp
+++ src/main.cpp
@@ -11,9 +11,6 @@ int main(int argc, char *argv[]) {
     std::cout << std::unitbuf;
     std::cerr << std::unitbuf;
 
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    std::cerr << "Logs from your program will appear here!" << std::endl;
-
     if (argc < 3) {
         std::cerr << "Usage: ./your_program tokenize <filename>" << std::endl;
         return 1;
@@ -24,13 +21,11 @@ int main(int argc, char *argv[]) {
     if (command == "tokenize") {
         std::string file_contents = read_file_contents(argv[2]);
         
-        // Uncomment this block to pass the first stage
-        // 
-        // if (!file_contents.empty()) {
-        //     std::cerr << "Scanner not implemented" << std::endl;
-        //     return 1;
-        // }
-        // std::cout << "EOF  null" << std::endl; // Placeholder, remove this line when implementing the scanner
+        if (!file_contents.empty()) {
+            std::cerr << "Scanner not implemented" << std::endl;
+            return 1;
+        }
+        std::cout << "EOF  null" << std::endl; // Placeholder, remove this line when implementing the scanner
         
     } else {
         std::cerr << "Unknown command: " << command << std::endl;
