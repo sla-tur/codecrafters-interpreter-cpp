93f1890deb355ddaec3a2adc79e572ec39f67f59
sla-tur <sla.tur1999@gmail.com>
2025-01-24 22:44:08 +0000

    adding comments and division

---
+++ src/Scanner.hpp
@@ -0,0 +1,12 @@
+#ifndef SCANNER_HPP
+#define SCANNER_HPP
+
+class Scanner {
+    private:
+};
+
+bool Scanner::isAtEnd() {
+    return current >= 
+}
+
+#endif // SCANNER_HPP
--- src/main.cpp
+++ src/main.cpp
@@ -14,27 +14,6 @@ char advance();
 //inline std::string token_type_to_string(TokenType type);
 //inline std::string anyToString(const std::any& value);
 
-/* enum class TokenType {
-    // Single-character tokens.
-    LEFT_PAREN, RIGHT_PAREN, LEFT_BRACE, RIGHT_BRACE,
-    COMMA, DOT, MINUS, PLUS, SEMICOLON, SLASH, STAR,
-
-    // One or two character tokens.
-    BANG, BANG_EQUAL,
-    EQUAL, EQUAL_EQUAL,
-    GREATER, GREATER_EQUAL,
-    LESS, LESS_EQUAL,
-
-    // Literals.
-    IDENTIFIER, STRING, NUMBER,
-
-    // Keywords.
-    AND, CLASS, ELSE, FALSE, FUN, FOR, IF, NIL, OR,
-    PRINT, RETURN, SUPER, THIS, TRUE, VAR, WHILE,
-
-    EOFILE
-}; */
-
 /*class Token {
     const TokenType     type;
     const std::string   lexeme;
