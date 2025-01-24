5d5596bf2993a4998e2c4721680d1f9780f7fb25
sla-tur <sla.tur1999@gmail.com>
2025-01-24 10:06:42 +0000

    working on lexing

--- src/main.cpp
+++ src/main.cpp
@@ -86,6 +86,24 @@ int main(int argc, char *argv[]) {
                     case '}':
                         std::cout << "RIGHT_BRACE } null" << std::endl;
                         break;
+                    case ',':
+                        std::cout << "COMMA , null" << std::endl;
+                        break;
+                    case '.':
+                        std::cout << "DOT . null" << std::endl;
+                        break;
+                    case '+':
+                        std::cout << "PLUS + null" << std::endl;
+                        break;
+                    case '-':
+                        std::cout << "MINUS - null" << std::endl;
+                        break;
+                    case ';':
+                        std::cout << "SEMICOLON ; null" << std::endl;
+                        break;
+                    case '*':
+                        std::cout << "STAR * null" << std::endl;
+                        break;
                 }
             }
         }
